17277f76f45585b4f0bd869ab1071aa801f3fbfe
yaweiwang <xdjm562@qq.com>
2023-11-16 17:18:30 +0800

    add logout on me_screen

--- lib/features/me/presentation/screens/me_screen.dart
+++ lib/features/me/presentation/screens/me_screen.dart
@@ -1,5 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture/routes/app_route.dart';
+import 'package:flutter_architecture/services/user_cache_service/domain/providers/user_cache_provider.dart';
+import 'package:flutter_architecture/shared/theme/app_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 @RoutePage()
@@ -9,12 +12,42 @@ class MeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Me"),
-      ),
-      body: const Center(
-        child: Text("Me"),
-      )
-    );
+        appBar: AppBar(
+          title: const Text("Me"),
+        ),
+        body: Center(
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            InkWell(
+              onTap: () async {
+                await ref.read(userLocalRepositoryProvider).deleteUser();
+                // ignore: use_build_context_synchronously
+                AutoRouter.of(context).pushAndPopUntil(
+                  LoginRoute(),
+                  predicate: (_) => false,
+                );
+              },
+              child: CircleAvatar(
+                child: Icon(
+                  Icons.logout,
+                  color: Theme.of(context).iconTheme.color,
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            InkWell(
+              onTap: () {
+                ref.read(appThemeProvider.notifier).toggleTheme();
+              },
+              child: CircleAvatar(
+                child: Icon(
+                  Theme.of(context).brightness == Brightness.dark
+                      ? Icons.light_mode
+                      : Icons.dark_mode,
+                  color: Theme.of(context).iconTheme.color,
+                ),
+              ),
+            )
+          ]),
+        ));
   }
 }
